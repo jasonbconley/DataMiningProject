be502f09ca2f07f4ed46a1b374ac2b159b5010e8
Jason Conley <70329226+jasonbconley@users.noreply.github.com>
2021-11-29 12:24:21 -0500

    Adding shiny app
    Running the shiny app will allow the user to input data about association rules and see support counts from that input

---
+++ EmailFraud/app.R
@@ -0,0 +1,79 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tm)
+library(corpus)
+library(SnowballC)
+library(Rcpp)
+library(ggplot2)
+library(wordcloud)
+
+# Here we set up the fraudulent email data set into a Corpus for processing
+# A corpus is a data structure that I had not seen before this text. Running
+# the command View(emailCorpus) after creating it will show the structure
+email_data <- read.csv("https://raw.githubusercontent.com/jasonbconley/DataMiningProject/main/fraud_email_.csv")
+colnames(email_data) <- c("text", "class")
+emailCorpusFrame <- corpus_frame(email_data)
+emailCorpus <- Corpus(VectorSource(emailCorpusFrame$text))
+
+# Here we strip the data of the things like whitespace and change all
+# characters to lower case. Essentially so that the tm dataset can work with
+# the data frame
+emailCorpus <- tm_map(emailCorpus, tolower)
+removeURL <- function(x) gsub("http[^[:space:]]*", "", x)
+emailCorpus <- tm_map(emailCorpus, removeURL)
+removeNumPunct <- function(x) gsub("[^[:alpha:][:space:]]*", "", x)
+emailCorpus <- tm_map(emailCorpus, removeNumPunct)
+replaceComma <- function(x) gsub(",", " ", x)
+emailCorpus <- tm_map(emailCorpus, replaceComma)
+conjunctions <- c("a", "that", "and", "are", "as", "at", "but", "for", "has", "the", "is", "to", "or", "this", "come", "also")
+myStopwords <- c(stopwords("en"), "available", "via", conjunctions)
+emailCorpus <- tm_map(emailCorpus, removeWords, myStopwords)
+emailCorpus <- tm_map(emailCorpus, removePunctuation)
+emailCorpus <- tm_map(emailCorpus, removeNumbers)
+emailCorpus <- tm_map(emailCorpus, stripWhitespace)
+
+# Now we can create the term document matrix, a collection of all terms and
+# the number of their occurrences in each document or email
+tdm <- TermDocumentMatrix(emailCorpus, control=list(wordLengths=c(2,Inf)))
+tdm <- removeSparseTerms(tdm, 0.9)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    
+    # Application title
+    titlePanel("Nigerian Letter Email Fraud"),
+    tags$head(tags$style(".leftAlign{float:left;}")),
+    verticalLayout(
+        mainPanel(
+           titlePanel("Most Popular Words in the dataset"),
+           plotOutput("wordCloud")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$wordCloud <- renderPlot({
+      pal <- brewer.pal(9, "BuGn")
+      pal <- pal[-(1:4)]
+      
+      # Creating a word cloud, we will use this to show the user the most frequent words
+      m <- as.matrix(tdm)
+      wordFreq <- sort(rowSums(m), decreasing=TRUE)
+      set.seed(123)
+      grayLevels <- gray( (wordFreq+10) / (max(wordFreq)+10) )
+      wordcloud(words=names(wordFreq), freq=wordFreq, random.order=F, colors=pal)
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
